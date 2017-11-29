b3cabad6d15f720ce41c12503e0fc0b3474db29a
Dmitry Edrenov <edrenoff@icloud.com>
2017-11-29 22:44:44 +0600

    Initial Commit

---
+++ Blocks/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Blocks
+//
+//  Created by Dmitry Edrenov on 29.11.17.
+//  Copyright © 2017 Dmitry Edrenov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
