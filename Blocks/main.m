86dd27f68eea1e3206bc33b067a9161a5b4be779
Dmitry Edrenov <edrenoff@icloud.com>
2019-02-17 15:56:27 +0600

    One Commit

--- Blocks/main.m
+++ Blocks/main.m
@@ -10,8 +10,41 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+       
+        // Создание массива строк, из которых удаляются гласные и контейнера для новых строк
+        NSArray *oldStrings = [NSArray arrayWithObjects:@"Sauerkraut", @"Raygun",
+        @"Big Nerd Ranch", @"Mississippi", nil];
+        
+        NSLog(@"Old strings: %@", oldStrings);
+        
+        NSMutableArray *newStrings = [NSMutableArray array];
+        
+        // Создание списка символов, удаляемых из строки
+        NSArray *vowels = [NSArray arrayWithObjects: @"a", @"e", @"i", @"o", @"u", nil];
+        
+        // Объявление блочной переменной
+        void (^devowelizer) (id, NSUInteger, BOOL *);
+        
+        // Присваивание блока переменной
+        devowelizer = ^(id string, NSUInteger i, BOOL *stop)
+        {
+            NSMutableString *newString = [NSMutableString stringWithString: string];
+            
+            // Перебор массива данных и замена всех вхождений элементов пустой строки
+            for (NSString *s in vowels)
+            {
+                NSRange fullRange = NSMakeRange(0, [newString length]);
+               
+                [newString replaceOccurrencesOfString: s withString: @"" options:
+                NSCaseInsensitiveSearch range: fullRange];
+            }
+            [newStrings addObject: newString];
+       
+        }; // Конец присваивания блока
+        
+        // Перебор элементов массива с блоком
+        [oldStrings enumerateObjectsUsingBlock: devowelizer];
+        NSLog(@"new strings: %@", newStrings);
     }
     return 0;
 }
